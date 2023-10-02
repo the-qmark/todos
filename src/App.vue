2fdcba4f696bfe55987469b71b946367247e4751
the-qmark <egormalyukow@gmail.com>
2023-10-02 13:45:21 +0300

    splitted the layout into components

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,23 @@
 <template>
-  <header class="app-header">
-    <span class="logo">TODOS</span>
-  </header>
+  <AppHeader />
 
-  <aside class="app-filters">
-    <section class="toggle-group">
-      <button class="button button--primary">All</button>
-      <button class="button">Active</button>
-      <button class="button">Done</button>
-    </section>
-  </aside>
+  <AppFilters />
 
   <main class="app-main">
-    <ul class="todo-list">
-      <li class="todo-item todo-item--done">
-        <div class="todo-item__status">
-          <i class="bi bi-check2"></i>
-        </div>
-        <span class="todo-item__text">Learn the basics of Vue</span>
-        <button class="todo-item__remove-button">
-          <i class="bi bi-trash3"></i>
-        </button>
-      </li>
-      <li class="todo-item">
-        <div class="todo-item__status">
-          <i class="bi bi-check2"></i>
-        </div>
-        <span class="todo-item__text">Learn the basics of Typescript</span>
-        <button class="todo-item__remove-button">
-          <i class="bi bi-trash3"></i>
-        </button>
-      </li>
-      <li class="todo-item">
-        <div class="todo-item__status">
-          <i class="bi bi-check2"></i>
-        </div>
-        <span class="todo-item__text">Subscribe to the channel</span>
-        <button class="todo-item__remove-button">
-          <i class="bi bi-trash3"></i>
-        </button>
-      </li>
-    </ul>
+    <AppTodoList />
 
-    <section class="add-todo">
-      <button class="add-todo__show-form-button">
-        <i class="bi bi-plus-lg"></i>
-      </button>
-      <form class="add-todo__form">
-        <button class="close-button" type="button">
-          <i class="bi bi-x"></i>
-        </button>
-        <div class="text-input text-input--focus">
-          <input class="input" />
-        </div>
-        <button class="button button--filled">Add task</button>
-      </form>
-    </section>
+    <AppAddTodo />
   </main>
 
-  <footer class="app-footer">2 more to do, 1 done</footer>
+  <AppFooter />
 </template>
 
 <script setup lang="ts">
+import AppHeader from './components/AppHeader.vue'
+import AppFilters from './components/AppFilters.vue'
+import AppTodoList from './components/AppTodoList.vue'
+import AppAddTodo from './components/AppTodoList.vue'
+import AppFooter from './components/AppFooter.vue'
 
 </script>
 
