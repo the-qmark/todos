006977bf32b8257c5a05ca649a92e0a685b0457f
the-qmark <egormalyukow@gmail.com>
2023-10-02 14:48:12 +0300

    add item and interface

---
+++ src/components/AppTodoItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <li
+    class="todo-item "
+    :class="{ 'todo-item--done': todo.completed }"
+  >
+    <div class="todo-item__status">
+      <i class="bi bi-check2"></i>
+    </div>
+    <span class="todo-item__text">{{ todo.text }}</span>
+    <button class="todo-item__remove-button">
+      <i class="bi bi-trash3"></i>
+    </button>
+  </li>
+</template>
+
+<script setup lang="ts">
+import { defineProps, PropType } from 'vue';
+import { Todo } from '@/types/Todo'
+
+
+const props = defineProps({
+  todo: {
+    type: Object as PropType<Todo>,
+    required: true
+  }
+})
+
+</script>
